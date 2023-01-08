f9bca3bbbe06742058707b9c6ab3d9604c2185f3
FrostyCow <dustbin96@gmail.com>
2023-01-08 18:21:06 +0800

    update with added schema.sql

--- SpringETL/springbatchv3/src/main/java/io/spring/Springbatchv3Application.java
+++ SpringETL/springbatchv3/src/main/java/io/spring/Springbatchv3Application.java
@@ -6,12 +6,14 @@ import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
@@ -21,19 +23,16 @@ public class Springbatchv3Application {
 //		SpringApplication.run(Springbatchv3Application.class, args);
 		
 		SpringApplication app = new SpringApplication(Springbatchv3Application.class);
-		ConfigurableApplicationContext ctx = app.run(args);
+		ApplicationContext ctx = app.run(args);
 		
 		JobLauncher jobLauncher = ctx.getBean(JobLauncher.class);
 		Job job = ctx.getBean("job", Job.class);
-		JobParameters jobParameters = new JobParametersBuilder().toJobParameters();
+		JobExplorer jobExplorer = ctx.getBean(JobExplorer.class);
+		JobParameters jobParameters = new JobParametersBuilder(jobExplorer)
+			.getNextJobParameters(job)
+			.toJobParameters();
 		
 		JobExecution jobExecution = jobLauncher.run(job, jobParameters);
-		BatchStatus batchStatus = jobExecution.getStatus();
-		
-//		TaskExecutorJobLauncher jobLauncher = new TaskExecutorJobLauncher();
-//		jobLauncher.run(new , null);
-//		
-//		JobLauncherApplicationRunner runner = new JobLauncherApplicationRunner(null, null, null)
 	}
 
 }
